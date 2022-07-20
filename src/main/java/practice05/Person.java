6abda9e20d4c8f89dbbf3b103887bf4a3b9d63e1
KiraYous <404465394@qq.com>
2022-07-20 16:23:18 +0800

    fix :refactor function of the teacher

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,16 @@
 package practice05;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
+    private String name;
+    private int age;
+
+
+    public String introduce() {
+        return  String.format("My name is %s. I am %d years old.",name,age);
+    }
 }
--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,37 @@
 package practice05;
 
-public class Teacher {
+import lombok.Data;
+
+
+public class Teacher extends Person {
+
+    private int klass=-1;
+
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        if(getKlass()==-1) return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.",getName(),getAge());
+
+        return  String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.",getName(),getAge(),getKlass());
+    }
+
+
 }
